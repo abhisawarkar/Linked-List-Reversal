1ba37137e2a5a57d263db656adca2c083a9e33d2
Abhishek Sawarkar <sawarkar.ab@husky.neu.edu>
2016-05-09 22:09:32 -0700

    Update Linked_List_Reversal.py

--- Linked_List_Reversal.py
+++ Linked_List_Reversal.py
@@ -1,27 +1,27 @@
 class Linkedlist:
 
-	def __init__(self):
+	def __init__(self):			# Initialize linked list
 		self.head= None
 
-	class Node:
+	class Node:				
 
-		def __init__(self,d):
+		def __init__(self,d):		# Initialize node
 			self.data = d
 			self.next= None
 
-	def add_beg(self,data):
+	def add_beg(self,data):			# Add node at beginning
 		New_node = self.Node(data)
 		New_node.next = self.head
 		self.head = New_node
 
-	def add_end(self,data):
+	def add_end(self,data):			# Add node at end
 		New_node = self.Node(data)
 		temp=self.head
 		while temp.next != None:
 			temp=temp.next		
 		temp.next = New_node
 
-	def reverse(self):
+	def reverse(self):			# Reverse a linked list
 		temp=self.head
 		New_LL=Linkedlist()
 		while temp:
@@ -31,20 +31,12 @@ class Linkedlist:
 			temp=temp.next
 		print New_LL.print_list()
 
-	def print_list(self):
+	def print_list(self):			# Print
 		temp=self.head
 		while temp:
 			print temp.data
 			temp = temp.next 
 
-LL=Linkedlist()
-LL.add_beg(2)
-LL.add_end(3)
-LL.add_beg(8)
-LL.add_end(1)
-LL.print_list()
-print "Reversing now"
-LL.reverse()
 
 
 
